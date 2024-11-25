46a9793c3075164a3713a55b727e067edcb7abb1
Eyanla Garcia <eygarcia@iu.edu>
2024-11-25 16:12:13 -0500

    the last of the code snippets added

--- finalProjectOutline/README.md
+++ finalProjectOutline/README.md
@@ -9,9 +9,13 @@ https://github.com/GarciaEy/N220/tree/main/finalProjectOutline
 ### Major Features
 
 - Allow users to add & remove tasks to the list or mark them as complete
+  - ![add and remove](image-3.png) this image shows how you can add and remove a "friend" which will be applied to the task manager assigment via adding and removing tasks
 - Allow users to change the text of the task and add any additonal properties to said task
+  - ![change properties](image-4.png)this image shows a function that changes a factor of the friends task. for the task manager assignment this will be applied via changing properties inside of each task.
 - user only sees tasks theyve created and make sure specific tasks are assigned to a specific user
+  - ![display function](image-1.png) this image is an example of an if-else statment being used to show difffernt displays to the user depending on whether or not theyre signed in. this can can be used in the task manager assignment by showing the user the login screen and the entry dashboard when theyre logged out/not signed in vs showing the user the task managment features when they hit the log in button and are logged in. the dash board can also be changed to say "welcome [username]".
 - Allow users to access a drop down menu that has the two options of indoor and outdoor tasks
+  - ![drop down js](image-5.png) this image is an example of how a drop down menu can be achived in java script -![drop down html](image-6.png) this image is an example of a how a drop down menu can be achived using html (and the js snippet above)
 
 **Login Form**
 
@@ -26,9 +30,8 @@ https://github.com/GarciaEy/N220/tree/main/finalProjectOutline
     - password contains no capital letters
     - password contains no numbers
 
-    - ![criteria function](image.png) this image is an example of an if-else statement being used to check that the user needs valid criteria to 'sign up' but for the case of the task manager assignment itll be the criteria for the login password listed above
-    - ![display function](image-1.png) this image is an example of an if-else statment being used to show difffernt displays to the user depending on whether or not theyre signed in. this can can be used in the task manager assignment by showing the user the login screen and the entry dashboard when theyre logged out/not signed in vs showing the user the task managment features when they hit the log in button and are logged in. the dash board can also be changed to say "welcome [username]".
-    - ![errors](image-2.png) lastly, this image is an example of if-else function statments being used to not only show if the user is signed up/logged in but to also communicate to the user and provide an error alert if there is an error with their login.
+      - ![criteria function](image.png) this image is an example of an if-else statement being used to check that the user needs valid criteria to 'sign up' but for the case of the task manager assignment itll be the criteria for the login password listed above
+      - ![errors](image-2.png) lastly, this image is an example of if-else function statments being used to not only show if the user is signed up/logged in but to also communicate to the user and provide an error alert if there is an error with their login.
 
 - Events:
   - on login button click check login
--- week8/friends.js
+++ week8/friends.js
@@ -1,104 +1,85 @@
-//reference variables
-
-const friendNameInput = document.getElementById("friendName");
+// Reference variables
+const friendNameInputRef = document.getElementById("friendName");
 const friendsListRef = document.getElementById("friendsList");
 
-//variables to track friends (are not in the html-name whatever)
+// Variable to track my friends
 const myFriends = [];
 
-//function for adding friend
+// Function for adding friend
 function addFriend() {
-  //get the name the user typed in
-  const nameValue = friendNameInput.value;
+  // get the name the user typed in
+  const nameValue = friendNameInputRef.value;
 
-  //add friend to myFriends list
-  myFriends.push(nameValue);
+  // add friend to myFriends list
+  myFriends.push({ name: nameValue, color: "black" });
 
   showFriends();
 
-  //add name to friends list
-  friendsListRef.innerHTML += `<li>
-  ${nameValue} <button onClick="removeFriend('${nameValue}')">X</button></button>
-  </li>`;
-
-  //clear the name the user types
-  friendNameInput.value = "";
+  // clear the name the user typed
+  friendNameInputRef.value = "";
 }
 
-function removeFriend(friendName) {
-  //   console.log(friendName);
-  //use variable to track index of friend to remove
-  let friendIndex;
-
-  //loop through current friends to find the friendName
-
-  for (let i = 0; i < myFriends.length; i++) {
-    if (myFriends[i] === friendName) {
-      friendIndex = i;
-    }
-  }
-
-  //remove the person from the list
+function removeFriend(friendIndex) {
+  // remove the person from the list
   myFriends.splice(friendIndex, 1);
+
   showFriends();
 }
 
-//function to loop through myFriends and show em on the page
-
+// function to loop through myFriends and show 'em on the page
 function showFriends() {
-  //Clear any friends currently on the page
+  // clear pages old friends list
   friendsListRef.innerHTML = "";
 
-  //loop through friends
-
-  myFriends.forEach(function (friendName) {
-    //add the name to the friends list
+  // loop through friends
+  myFriends.forEach(function (friend, friendIndex) {
+    // add the name to the friends list
     friendsListRef.innerHTML += `<li>
-  	${friendName} 
-	<button onClick="removeFriend('${friendName}')">X</button> 
-	<button onClick="moveFriend('${friendName}', true)">	&#8593;</button>
-	<button onClick="moveFriend('${friendName}', false)>	&#8595;</button>
-  </li>`;
+    <span style="color: ${friend.color}">${friend.name}</span>
+    <button onclick="removeFriend(${friendIndex})">X</button>
+    <button onclick="moveFriend(${friendIndex}, true)">&#8593;</button>
+    <button onclick="moveFriend(${friendIndex}, false)">&#8595;</button>
+    
+    <input 
+        type="color" 
+        name="friendTextColor${friendIndex}"
+        id="friendTextColor${friendIndex}"
+        onchange="changeFriendTextColor(${friendIndex}, event.currentTarget)"
+    />
+    
+ </li>`;
   });
 }
 
-//function to clear list of friends
+function changeFriendTextColor(friendIndex, inputRef) {
+  const friendName = myFriends[friendIndex].name;
+  myFriends.splice(friendIndex, 1, { name: friendName, color: inputRef.value });
 
+  showFriends();
+}
+
+// function to clear list of friends
 function clearList() {
   myFriends.splice(0, myFriends.length);
 
   showFriends();
 }
 
-//function to move friends ranking
-
-function moveFriend(friendName, isMovingUp) {
-  let friendIndex;
+// function to move friends ranking
+function moveFriend(friendIndex, isMovingUp) {
   let friendNewIndex;
-  for (let i = 0; i < myFriends.length; i++) {
-    if (myFriends[i] === friendName) {
-      friendIndex = i;
-      //['A', 'B', 'C'] move 'B' up 1 space
-      //index of 'B' is originally 1
-      //this is what the result should be
-      //['B', 'A', 'C']so the new index would be 0
 
-      if (isMovingUp) {
-        friendIndex = friendNewIndex - 1;
-      }
-
-      //['A', 'B', 'C'] move 'B' down 1 space
-      //index of 'B' is originally 1
-      //this is what the result should be
-      //['A', 'C', 'B']so the new index would be 2
-      else {
-        friendNewIndex = friendIndex + 1;
-      }
-    }
+  if (isMovingUp) {
+    friendNewIndex = friendIndex - 1;
+  } else {
+    friendNewIndex = friendIndex + 1;
   }
 
+  const friendName = myFriends[friendIndex].name;
+  // remove friend from old space
   myFriends.splice(friendIndex, 1);
-  myFriends.splice(friendNewIndex, 0, friendName);
+  // add friend to new space
+  myFriends.splice(friendNewIndex, 0, { name: friendName, color: "black" });
 
   showFriends();
 }
