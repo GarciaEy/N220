10998ce536457fd7640fe1bdd3b4e6d4f09c3512
Eyanla Garcia <eygarcia@iu.edu>
2024-12-03 22:26:21 -0500

    Homework 7 revised

--- homework-seven/broken_JS.js
+++ homework-seven/broken_JS.js
@@ -7,34 +7,36 @@ let innerscore = 0;
 function RPS(numb) {
   let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
 
-  document.getElementById("resultdecision") =
-    "The computer chose:" + Boolean(CompDecision);
-  if (numb = 3) {
+  document.getElementById("resultDecision") ==
+    "The computer chose:" + CompDecision;
+
+  if (numb == 3) {
     innerscore -= 0.5;
-  }else if ((numb == 2)) {
+  } else if (numb === 0) {
     if (CompDecision == "Paper") {
       innerscore -= 1;
-    }else if (CompDecision == "Scissors") {innerscore += 1;
+    } else if (CompDecision == "Scissors") {
+      innerscore += 1;
     }
-  }else if ((numb == 3)) {
+  } else if (numb == 1) {
     if (CompDecision == "Rock") {
       innerscore += 1;
-    }else if (CompDecision == "Scissors") {
+    } else if (CompDecision == "Scissors") {
       innerscore -= 1;
     }
   }
 
-  if ((numb == 1)) {
+  if (numb == 2) {
     if (resultDecision == "Rock") {
       innerscore -= 1;
     } else {
-      resultDecision == "Paper"
+      resultDecision == "Paper";
       innerscore += 1;
     }
-  } 
+  }
+  document.getElementById("Score").innerHTML = innerscore;
 }
 
-
-document.getElementById("Score").innerHTML = innerscore;
+// document.getElementById("Score").innerHTML = innerscore;
 
 // }
